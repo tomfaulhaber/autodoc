e47e8571338958a4df66edc2c57cf3e74c24bf09
Tom Faulhaber <git_net@infolace.com>
2009-06-25 10:50:46 -0700

    First support for building HTML output with enlive templates.

---
+++ com/infolace/gen_docs/build_html.clj
@@ -0,0 +1,18 @@
+(ns com.infolace.gen-docs.build-html
+  (:refer-clojure :exclude [empty complement]) 
+  (:use net.cgrand.enlive-html
+        [clojure.contrib.duck-streams :only (with-out-writer)]))
+
+;; TODO: consolidate and DRY defs
+(def *file-prefix* "../wiki-work-area/")
+(def *output-directory* (str *file-prefix* "wiki-src/"))
+
+(def *overview-file* "overview.html")
+
+(deftemplate overview *overview-file* [ns-info]
+  [:.toc-entry] (clone-for [ns ns-info] (content (:short-name ns))))
+
+(defn make-overview [ns-info]
+  (with-out-writer (str *output-directory* *overview-file*) 
+    (print (apply str (overview ns-info)))))
+
