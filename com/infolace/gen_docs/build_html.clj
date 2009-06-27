e2bf808c8e1249a50303b22985105d9db0a66ce4
Tom Faulhaber <git_net@infolace.com>
2009-06-26 23:35:29 -0700

    Do some overview doc for each namespace.

--- com/infolace/gen_docs/build_html.clj
+++ com/infolace/gen_docs/build_html.clj
@@ -1,6 +1,7 @@
 (ns com.infolace.gen-docs.build-html
   (:refer-clojure :exclude [empty complement]) 
   (:use net.cgrand.enlive-html
+        [clojure.contrib.pprint.utilities :only (prlabel)]
         [clojure.contrib.duck-streams :only (with-out-writer)]))
 
 ;; TODO: consolidate and DRY defs
@@ -9,12 +10,29 @@
 
 (def *overview-file* "overview.html")
 
+(defn ns-html-file [ns-info]
+  (str (:short-name ns-info) "-api.html"))
+
 (deftemplate overview *overview-file* [ns-info]
   [:.toc-entry] (clone-for [ns ns-info]
                            #(at % [:a] 
                                 (do->
                                  (set-attr :href (str "#" (:short-name ns)))
-                                 (content (:short-name ns))))))
+                                 (content (:short-name ns)))))
+  [:ul#left-sidebar-list] (clone-for [ns ns-info]
+                                     #(at % [:a] 
+                                          (do->
+                                           (set-attr :href (ns-html-file ns))
+                                           (content (:short-name ns)))))
+  [:div#namespace-entry] (clone-for [ns ns-info]
+                                    #(at % 
+                                         [:#namespace-tag] 
+                                         (do->
+                                          (set-attr :id (:short-name ns))
+                                          (content (:short-name ns)))
+                                         [:#author] (content (or (:author ns) "unknown author"))
+                                         [:a#api-link] (set-attr :href (ns-html-file ns))
+                                         [:code#namespace-docstr] (content (:doc ns)))))
 
 (defn make-overview [ns-info]
   (with-out-writer (str *output-directory* *overview-file*) 
