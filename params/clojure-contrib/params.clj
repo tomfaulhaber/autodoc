57491b3619aa0be5593a2cda136b4f201b555baf
Tom Faulhaber <git_net@infolace.com>
2009-11-12 09:49:16 -0800

    Just a holder file so we can think about getting contrib to work

---
+++ params/clojure-contrib/params.clj
@@ -0,0 +1,11 @@
+
+(def *load-except-list* 
+     [ 
+      #"/test_contrib"
+      #"/test_clojure"
+      #"/load_all"
+      #"/datalog/tests/"
+      #"/datalog/example"
+      #"/javadoc"
+      #"/jmx/Bean"
+      ])
