9919adc8fb40e0cfbdde54b976770221d5eb7a5f
Tom Faulhaber <git_net@infolace.com>
2009-12-17 09:46:43 -0800

    Convert ^x to (meta x) because ^x is now deprecated

--- com/infolace/gen_docs/collect_info.clj
+++ com/infolace/gen_docs/collect_info.clj
@@ -30,27 +30,27 @@ string, which looks nasty when you display it."
   "Determing the type (var, function, macro) of a var from the metadata and
 return it as a string."
   [v]
-  (cond (:macro ^v) "macro"
-        (= (:tag ^v) clojure.lang.MultiFn) "multimethod"
-        (:arglists ^v) "function"
+  (cond (:macro (meta v)) "macro"
+        (= (:tag (meta v)) clojure.lang.MultiFn) "multimethod"
+        (:arglists (meta v)) "function"
         :else "var"))
 
 (defn vars-for-ns [ns]
   (for [v (sort-by (comp :name meta) (vals (ns-interns ns)))
-        :when (and (or (:wiki-doc ^v) (:doc ^v)) (not (:skip-wiki ^v)) (not (:private ^v)))] v))
+        :when (and (or (:wiki-doc (meta v)) (:doc (meta v))) (not (:skip-wiki (meta v))) (not (:private (meta v))))] v))
 
 (defn vars-info [ns]
   (for [v (vars-for-ns ns)] 
-    (merge (select-keys ^v [:arglists :file :line])
-           {:name (name (:name ^v))
-            :doc (remove-leading-whitespace (:doc ^v)),
+    (merge (select-keys (meta v) [:arglists :file :line])
+           {:name (name (:name (meta v)))
+            :doc (remove-leading-whitespace (:doc (meta v))),
             :var-type (var-type v)})))
 
 (defn add-vars [ns-info]
   (merge ns-info {:members (vars-info (:ns ns-info))}))
 
 (defn relevant-namespaces []
-  (filter #(not (:skip-wiki ^%))
+  (filter #(not (:skip-wiki (meta %)))
           (map #(find-ns (symbol %)) 
                (filter #(some (fn [n] (or (= % n) (.startsWith % (str n "."))))
                               *namespaces-to-document*)
@@ -68,7 +68,7 @@ return it as a string."
    (drop-while 
     (comp not identity) 
     (map #(let [ns-part (find-ns (symbol %))]
-            (if (not (:skip-wiki ^ns-part))
+            (if (not (:skip-wiki (meta ns-part)))
               ns-part))
          (let [parts (seq (.split (name (ns-name ns)) "\\."))]
            (map #(apply str (interpose "." (take (inc %) parts)))
@@ -84,15 +84,15 @@ have the same prefix followed by a . and then more components"
   (let [pat (re-pattern (str (.replaceAll (name (ns-name ns)) "\\." "\\.") "\\..*"))]
     (sort-by
      #(name (ns-name %))
-     (filter #(and (not (:skip-wiki ^%)) (re-matches pat (name (ns-name %)))) (all-ns)))))
+     (filter #(and (not (:skip-wiki (meta %))) (re-matches pat (name (ns-name %)))) (all-ns)))))
 
 (defn ns-short-name [ns]
   (trim-ns-name (name (ns-name ns))))
 
 (defn build-ns-entry [ns]
   {:full-name (name (ns-name ns)) :short-name (ns-short-name ns)
-   :doc (remove-leading-whitespace (:doc ^ns)) :author (:author ^ns)
-   :see-also (:see-also ^ns) :ns ns})
+   :doc (remove-leading-whitespace (:doc (meta ns))) :author (:author (meta ns))
+   :see-also (:see-also (meta ns)) :ns ns})
 
 (defn build-ns-list [nss]
   (sort-by :short-name (map add-vars (map build-ns-entry nss))))
