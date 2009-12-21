97ecfe6b39d1f130976dcef8d573f3319f73f8a7
Tom Faulhaber <git_net@infolace.com>
2009-12-20 23:51:56 -0800

    First leiningen build file

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject autodoc "0.3.0-SNAPSHOT"
+  :description "A tool to build HTML documentation from your Clojure source"
+  :url "http://github.com/tomfaulhaber/autodoc"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+                 [enlive "1.0.0-SNAPSHOT"]
+                 [ant/ant "1.6.2"]
+                 [ant/ant-launcher "1.6.2"]])
