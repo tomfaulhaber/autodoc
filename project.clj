a212d67e1272048881b1025d8d14b459042c753e
Tom Faulhaber <git_net@infolace.com>
2010-01-13 23:34:49 -0800

    marked for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autodoc "0.7.0-SNAPSHOT"
+(defproject autodoc "0.7.0"
   :description "A tool to build HTML documentation from your Clojure source"
   :url "http://github.com/tomfaulhaber/autodoc"
   :main autodoc.autodoc
