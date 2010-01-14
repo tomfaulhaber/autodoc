d8354b621ac6be93893d652696a7412ba012bbde
Tom Faulhaber <git_net@infolace.com>
2010-01-13 23:20:20 -0800

    Update version and clean dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject autodoc "0.3.0-SNAPSHOT"
+(defproject autodoc "0.7.0-SNAPSHOT"
   :description "A tool to build HTML documentation from your Clojure source"
   :url "http://github.com/tomfaulhaber/autodoc"
   :main autodoc.autodoc
@@ -6,8 +6,6 @@
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [enlive "1.0.0-SNAPSHOT"]
                  [ant/ant "1.6.2"]
-                 [swank-clojure "1.1.0"] ; testing
-                 [org.apache.maven/maven-ant-tasks "2.0.10"] ; testing
                  [ant/ant-launcher "1.6.2"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]])
