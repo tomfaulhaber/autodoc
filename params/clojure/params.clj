6b497c7112014efd337b3a0cf5cc95f2ed53c554
Tom Faulhaber <git_net@infolace.com>
2010-02-17 10:23:47 -0800

    Reformatted and changed the master build to use the build product itself

--- params/clojure/params.clj
+++ params/clojure/params.clj
@@ -2,33 +2,35 @@
 
 (let [file-prefix (.getAbsolutePath (File. "../autodoc-work-area/clojure"))
       root (str file-prefix "/src/")]
- {
-  :name "Clojure",
-  :file-prefix file-prefix,
-  :root root,
-  :source-path "src/clj",
-  :web-src-dir "http://github.com/richhickey/clojure/blob/",
+  {
+   :name "Clojure",
+   :file-prefix file-prefix,
+   :root root,
+   :source-path "src/clj",
+   :web-src-dir "http://github.com/richhickey/clojure/blob/",
 
-  :web-home "http://richhickey.github.com/clojure-contrib/",
-  :output-path (str file-prefix "/autodoc/"),
-  :external-doc-tmpdir "/tmp/autodoc/doc",
-  :jar-file (str root "clojure-slim.jar"),
+   :web-home "http://richhickey.github.com/clojure-contrib/",
+   :output-path (str file-prefix "/autodoc/"),
+   :external-doc-tmpdir "/tmp/autodoc/doc",
+   :jar-file (str root "clojure-slim.jar"),
 
-  :built-clojure-jar nil,
+   :built-clojure-jar nil,
 
-  :namespaces-to-document ["clojure.core" "clojure.inspector" "clojure.main" 
+   :namespaces-to-document ["clojure.core" "clojure.inspector" "clojure.main" 
                             "clojure.set" "clojure.stacktrace" "clojure.template"
                             "clojure.test" "clojure.walk" "clojure.xml"
                             "clojure.zip"],
 
-  :branches [["master" {:built-clojure-jar "/home/tom/src/clj/clojure-master/clojure.jar"}],
-             ["1.1.x" {}]]
-  :load-except-list 
-  [ 
-   #"clojure/core.clj"
-   #"clojure/parallel.clj"
-   ],
+   :branches [["master" 
+               {:built-clojure-jar
+                "/home/tom/src/clj/autodoc-work-area/clojure/src/clojure-slim.jar"}],
+              ["1.1.x" {}]]
+   :load-except-list 
+   [ 
+    #"clojure/core.clj"
+    #"clojure/parallel.clj"
+    ],
 
-  :page-title "Clojure Core API Reference",
-  :copyright "Copyright 2007-2009 by Rich Hickey",
-  })
+   :page-title "Clojure Core API Reference",
+   :copyright "Copyright 2007-2009 by Rich Hickey",
+   })
