0103749aa905dc20ab087d1ab84fdbe0365a0aad
Tom Faulhaber <git_net@infolace.com>
2010-06-29 00:20:45 -0700

    Use the new command line param dir in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp autodoc-standalone.jar clojure.main -e "(do (use 'autodoc.gen-docs) (gen-docs \"params/$1\" true))"
+java -jar autodoc-standalone.jar --param-dir=params/$1 --commit?=true
