3f43594657820214d52c06da479fbbdb87ccfd6e
Tom Faulhaber <git_net@infolace.com>
2010-07-01 00:03:39 -0700

    Explicitly include the namespace for compilation so we get a main out of it

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A tool to build HTML documentation from your Clojure source"
   :url "http://github.com/tomfaulhaber/autodoc"
   :main autodoc.autodoc
+  :namespaces [autodoc.autodoc]
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [enlive "1.0.0-SNAPSHOT"]]
