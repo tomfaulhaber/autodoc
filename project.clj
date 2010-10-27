d049788935aaf1ce8b19da270d9a2d9ae59d40c5
Tom Faulhaber <git_net@infolace.com>
2010-10-26 23:26:47 -0700

    Changed swank version

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [enlive "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.2.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]])
