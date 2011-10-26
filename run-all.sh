1c532580c1e0def270c0a0320cbdd44f30dd3850
Tom Faulhaber <git_net@infolace.com>
2011-10-25 23:55:43 -0700

    Support running only contrib

--- run-all.sh
+++ run-all.sh
@@ -3,6 +3,13 @@
 projects="clojure incanter"
 contribs="algo.monads data.json java.classpath java.jdbc tools.logging tools.macros tools.nrepl"
 
+if [ $# -ge 1 ]; then
+    if [ "$1" == "contrib" ]; then
+        shift
+        projects=""
+    fi
+fi
+
 if [ $# -lt 1 ]; then 
     commit=true
 else
