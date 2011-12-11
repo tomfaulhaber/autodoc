a549692d08e14126d8853a82c408c6344e895a8d
Tom Faulhaber <git_net@infolace.com>
2011-12-10 22:29:49 -0800

    Support for more contribs

--- params/contrib.clj
+++ params/contrib.clj
@@ -36,7 +36,8 @@
             args)]))
 
 (into {}
-      [(! :project "algo.monads" :name "Monad Macros and Definitions"
+      [(! :project "algo.generic" :name "Generic versions of common functions")
+       (! :project "algo.monads" :name "Monad Macros and Definitions"
           :branches [{:name "master"
                       :version :from-pom
                       :status "in development"
@@ -44,10 +45,38 @@
                                               ['org.clojure/tools.macro "0.1.1"]]}},
                      ]
           )
+       (! :project "core.incubator" :name "Functions proposed for core inclusion")
+       (! :project "core.match" :name "Pattern match and predicate dispatch")
+       (! :project "core.unify" :name "Unification library")
        (! :project "data.codec" :name "Native codec implementations")
-       (! :project "data.json" :name "JSON Utilities")
+       (! :project "data.csv" :name "Reading and writing CSV files")
+       (! :project "data.finger-tree" :name "Finger trees")
+       (! :project "data.json" :name "JSON Utilities") 
+       (! :project "data.priority-map" :name "Priority maps")
+       (! :project "data.xml" :name "Lazy XML parsing")
+       (! :project "data.zip" :name "Manipulating zippers")
        (! :project "java.classpath" :name "Classpath Utilities")
+       (! :project "java.data" :name "Work with Java Beans"
+          :branches [{:name "master"
+                      :version :from-pom
+                      :status "in development"
+                      :params {:dependencies [['org.clojure/clojure "1.3.0"]
+                                              ['org.clojure/tools.logging "0.2.3"]]}},
+                     ])
        (! :project "java.jdbc" :name "JDBC-based SQL Interface")
+       (! :project "java.jmx" :name "JMX Interface")
+       (! :project "math.combinatorics" :name "Lazy sequences for common combinatorial functions")
+       (! :project "math.numeric-tower" :name "Math functions")
+       (! :project "test.benchmark" :name "Benchmark suite")
+       (! :project "tools.cli" :name "Command-line processor")
        (! :project "tools.logging" :name "Logging Tools")
        (! :project "tools.macros" :name "Tools for Macro Writers")
+       (! :project "tools.namespace" :name "Extract namespace declarations"
+          :branches [{:name "master"
+                      :version :from-pom
+                      :status "in development"
+                      :params {:dependencies [['org.clojure/clojure "1.3.0"]
+                                              ['org.clojure/java.classpath "0.1.1"]]}},
+                     ])
+       (! :project "tools.trace" :name "Execution tracing tool")
        (! :project "tools.nrepl" :name "Network REPL")])
