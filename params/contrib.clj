d667fa75c3d49bd7bba2d8aac9d700ec10e8e112
Tom Faulhaber <git_net@infolace.com>
2011-12-10 23:12:05 -0800

    Don't use the description of java.jdbc by default

--- params/contrib.clj
+++ params/contrib.clj
@@ -36,7 +36,8 @@
             args)]))
 
 (into {}
-      [(! :project "algo.generic" :name "Generic versions of common functions")
+      [(! :project "algo.generic" :name "Generic versions of common functions"
+          :description "Generic versions of commonly used functions, implemented as multimethods that can be implemented for any data type.")
        (! :project "algo.monads" :name "Monad Macros and Definitions"
           :branches [{:name "master"
                       :version :from-pom
