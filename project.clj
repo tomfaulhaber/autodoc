d79724a0970bfedfac1ed3bee1e0f14931224ba1
Tom Faulhaber <git_net@infolace.com>
2013-04-14 12:03:45 -0700

    Change dependency to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/tomfaulhaber/autodoc"
   :main autodoc.autodoc
   :aot [autodoc.autodoc]
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/data.zip "0.1.0"]
                  [org.clojure/tools.namespace "0.1.0"]
