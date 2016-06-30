672de3773023c330662089ea5f14f43cc4202add
Tom Faulhaber <git_com@infolace.com>
2016-06-30 11:51:28 -0700

    Track to latest alpha of Clojure 1.9

--- src/autodoc/pom_tools.clj
+++ src/autodoc/pom_tools.clj
@@ -31,7 +31,7 @@ removed that suffix and false otherwise"
   [deps]
   (if (some #(= "clojure" (-> % first name)) deps)
     deps
-    (conj deps ['org.clojure/clojure "1.9.0-alpha4"])))
+    (conj deps ['org.clojure/clojure "1.9.0-alpha8"])))
 
 (defn get-dependencies
   "If dep-param is :from-pom, returns the dependencies from the pom as a
