15cda8a97aeb68adb4289cbef095896f6f074244
Tom Faulhaber <git_com@infolace.com>
2016-06-30 11:55:40 -0700

    Update dependency on autodoc-collect

--- project.clj
+++ project.clj
@@ -9,5 +9,5 @@
                  [org.clojure/tools.namespace "0.2.11"]
                  [enlive "1.0.0" :exclusions [org.clojure/clojure]]
                  [leiningen-core "2.5.2"]
-                 [autodoc/autodoc-collect "1.1.2"]]
+                 [autodoc/autodoc-collect "1.1.3"]]
   :profiles {:uberjar {:aot :all}})
