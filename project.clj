b351e4d28c1c3cbd6353442395f4b70bcc53cb63
Tom Faulhaber <git_com@infolace.com>
2016-07-01 12:30:58 -0700

    Add info to project.clj to support release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject autodoc "1.1.1-SNAPSHOT"
   :description "A tool to build HTML documentation from your Clojure source"
   :url "http://github.com/tomfaulhaber/autodoc"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main autodoc.autodoc
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.memoize "0.5.6"]
@@ -10,4 +12,5 @@
                  [enlive "1.0.0" :exclusions [org.clojure/clojure]]
                  [leiningen-core "2.5.2"]
                  [autodoc/autodoc-collect "1.1.3"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:uberjar {:aot :all}})
