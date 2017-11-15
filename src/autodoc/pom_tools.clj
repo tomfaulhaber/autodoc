a8686531934f7279bc6736879204d82ba1dc5471
puredanger <alex.miller@cognitect.com>
2017-11-15 15:26:28 -0600

    update to latest clojure when running

--- src/autodoc/pom_tools.clj
+++ src/autodoc/pom_tools.clj
@@ -31,7 +31,7 @@ removed that suffix and false otherwise"
   [deps]
   (if (some #(= "clojure" (-> % first name)) deps)
     deps
-    (conj deps ['org.clojure/clojure "1.9.0-alpha16"])))
+    (conj deps ['org.clojure/clojure "1.9.0-RC1"])))
 
 (defn get-dependencies
   "If dep-param is :from-pom, returns the dependencies from the pom as a
